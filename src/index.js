c52d23df79d859e01ba94c3d92c50dea833b7fa3
ratskin <elliot.schep@gmail.com>
2019-01-09 12:18:33 +0100

    Removed sample code, added fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Inject global styles
+const GlobalStyle = createGlobalStyle`
+  @import url('./assets/fonts/Aileron/Aileron-Regular.otf');
+  @import url('./assets/fonts/Big\ John\ Slim\ Joe/BIG\ JOHN.otf');
+  @import url('./assets/fonts/Finland/Finland\ Rounded\ Thin.otf');
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: #E1605F;
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    font-family: 'Aileron', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
