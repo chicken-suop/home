d1774c90a36dd7107e005ed9d1a82cab19d5e39f
ratskin <elliot.schep@gmail.com>
2019-01-14 17:38:02 +0100

    Added Paralax

--- src/helpers/styledComponentsConfig.js
+++ src/helpers/styledComponentsConfig.js
@@ -1,12 +1,14 @@
 import { css } from 'styled-components';
 
 export const theme = {
-  boxShadow: '0 10px 40px rgba(0, 0, 0, 0.2)',
-  color: 'rgba(0, 0, 0, 0.87)',
-  whiteColor: 'rgba(255, 255, 255, 0.87)',
+  colours: {
+    red: '#E1605F',
+    grey: '#DFDFDF',
+    blue: '#020B3C',
+  },
 };
 
-export const sizes = {
+const sizes = {
   desktop: 992,
   tablet: 768,
   phone: 576,
